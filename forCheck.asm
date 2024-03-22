aa90932699595aa2e52eb2b66cae2c6625be4013
MaFiN1337 <antonpigulak@gmail.com>
2024-03-22 13:32:48 +0200

    TASM commit. Modify Comparing of string procedure
    Now result is on console(when run ASM code)

--- forCheck.asm
+++ forCheck.asm
@@ -2,30 +2,33 @@
 .stack 100h      
 
 .data
-    str1         DB  "3466"
-    lenStr1      equ ($-str1)
-    str2         db  "346", 0
-    lenStr2      equ ($-str2)
-    oneChar      db  ?
-    filename     DB  "test.in", 0
-    varForAX     DW  ?
-    binaryString db  16 dup(?)
+    str1           DB  "3466", 0
+    lenStr1        equ ($-str1)
+    str2           db  "3466", 0
+    lenStr2        equ ($-str2)
+    oneChar        db  ?
+    filename       DB  "test.in", 0
+    varForAX       DW  ?
+    binaryString   db  16 dup(?)
+    firstIsBigger  DB  "First is bigger", "$"
+    secondIsBigger DB  "Second is bigger", "$"
+    equal          DB  "Strings are equal", "$"
 
 .code
 main proc
                    mov  ax, @data
                    mov  ds, ax
-    ;mov di, 0
+    ;mov  di, 0
     ;read_next:
-    ;inc di
-    ;mov ah, 3Fh
-    ; mov bx, 0h                ; stdin handle
-    ; mov cx, 1                 ; 1 byte to read
-    ;mov dx, offset oneChar    ; read to ds:dx
-    ;int 21h                   ;  ax = number of bytes read
-    ;or  ax,ax
-    ;jnz read_next
-    ;lea si, oneChar
+    ; inc  di
+    ; mov  ah, 3Fh
+    ; mov  bx, 0h                                   ; stdin handle
+    ; mov  cx, 2                                    ; 1 byte to read
+    ;mov  dx, offset oneChar                       ; read to ds:dx
+    ; int  21h                                      ;  ax = number of bytes read
+    ;or   ax,ax
+    ;jnz  read_next
+    ;lea  si, oneChar
             
     ; write_to_console:
     ;            mov ah, 02h
@@ -35,8 +38,9 @@ main proc
     ;            dec di
     ;            cmp di, 0
     ;            jne write_to_console
-                   call Converting                  ; result in BX
-                   call bxToBinary
+    ;call Converting                  ; result in BX
+    ;call bxToBinary
+                   call Comparing
                    mov  ah, 4Ch
                    int  21h
 
@@ -93,19 +97,20 @@ Exponentiation endp
 bxToBinary PROC
                    neg  bx
                    mov  cx, 16
-                   lea  di, binaryString            
+                   lea  di, binaryString
 
-    nextBitLoop:      
-                   shl  bx, 1                      
-                   jnc  zero_bit                    
-                   mov  byte ptr [di], '1'          
-                   jmp  next_bit
-    zeroBit:      
-                   mov  byte ptr [di], '0'          
-                   and  byte ptr [di], 11111110b    
-    nextBit:      
-                   inc  di                         
+    nextBitLoop:   
+                   shl  bx, 1
+                   jnc  zeroBit
+                   mov  byte ptr [di], '1'
+                   jmp  nextBit
+    zeroBit:       
+                   mov  byte ptr [di], '0'
+                   and  byte ptr [di], 11111110b
+    nextBit:       
+                   inc  di
                    loop nextBitLoop
+                   mov  byte ptr [di], '$'
                    mov  ah, 9
                    int  21h
                    ret
@@ -114,28 +119,46 @@ bxToBinary endp
 
 
 
-    ;Comparing PROC
-    ;mov ax, lenStr1
-    ; cmp ax, lenStr2
-    ; jne notEqual
+Comparing PROC
+                   mov  ax, lenStr1
+                   cmp  ax, lenStr2
+                   jne  notEqual
 
-    ; mov cx, lenStr1
-    ; mov si, offset str1
-    ; mov di, offset str2
-    ;  cld
-    ;  compareLoop:
-    ;     mov AL, [SI]
-    ;      inc si
-    ;     mov Bl, [DI]
-    ;      inc di
-    ;     cmp al, bl
-    ;    jne notEqual
-    ;     dec cx
-    ;     jnz compareLoop
-    ;      mov bx, 10
-    ;    ret
-    ; notEqual:
-    ;        mov bx, 6
-    ;         ret
-    ;Comparing endp
+                   mov  cx, lenStr1
+                   mov  si, offset str1
+                   mov  di, offset str2
+                   cld
+    compareLoop:   
+                   mov  AL, [SI]
+                   inc  si
+                   mov  Bl, [DI]
+                   inc  di
+                   cmp  al, bl
+                   jne  lensDiffer
+                   dec  cx
+                   jnz  compareLoop
+                   mov  dx, offset equal
+                   mov  ah, 9
+                   int  21h
+                   ret
+    notEqual:      
+                   cmp  al, bl
+                   jg   FirstBigger
+                   jmp  SecondBigger
+
+    FirstBigger: 
+                   mov  dx, offset firstIsBigger
+                   mov  ah, 9
+                   int  21h
+                   ret
+    SecondBigger:
+                   mov  dx, offset secondIsBigger
+                   mov  ah, 9
+                   int  21h
+                   ret
+    lensDiffer:    
+                   cmp  ax, lenStr2
+                   jg   firstBigger
+                   jmp  secondBigger
+Comparing endp
 end main
